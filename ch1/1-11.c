6e7d817b2866dcf3df1c7b7df67d662772c40c93
Kenty Wang <kentymail@gmail.com>
2019-09-02 09:17:01 -0400

    1-11

---
+++ ch1/1-11.c
@@ -0,0 +1,5 @@
+// Test each branch of execution possible, given the if statements and ||
+// choices.
+
+// The inputs most likely to produce bugs are ones that reach seldom
+// encountered branches.
