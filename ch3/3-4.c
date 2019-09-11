bcd4f95381f9897b53a4a1b022a4541203f250c0
Kenty Wang <kentymail@gmail.com>
2019-09-11 09:27:55 -0400

    3-4

---
+++ ch3/3-4.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+/*
+00 (0)
+01 (1)
+10 (-2)
+11 (-1)
+
+The book example of itoa can't handle the largest negative number of because
+it converts any negative number to positive, meaning the largest negative
+would get converted to 0.
+
+To counter this, we would just leave n untouched but take the absolute value
+of the modulo within the do-while loop. And we need to make sure the while
+condition accepts that n is less than zero.
+*/
+
+void itoa(char n, char s[]);
+
+int main()
+{
+  char s[MAXLINE];
+  itoa(-128, s);
+  printf("%s\n", s);
+
+  return 0;
+}
+
+void itoa(char n, char s[])
+{
+  int i, m;
+  int sign = n;
+
+  i = 0;
+  do {
+    m = n % 10;
+    
+    if (m < 0)
+      m = -m;
+
+    s[i++] = m + '0';
+
+  } while ((n /= 10) != 0);
+
+  if (sign < 0)
+    s[i++] = '-';
+
+  s[i] = '\0';
+}
